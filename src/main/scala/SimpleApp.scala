4e94de9068da9a3b5503edb67bd5742537adc192
Thomas Bénas <benas.thomas@gmail.com>
2024-02-20 15:42:14 +0100

    [clean] nettoyage du code

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -4,11 +4,35 @@ import org.apache.spark.sql.functions.{col, when}
 import org.apache.spark.sql.types.{BooleanType, DateType, TimestampType}
 import org.apache.spark.sql.jdbc.{JdbcDialect, JdbcDialects, JdbcType}
 import org.apache.spark.sql.types._
+import java.util.Properties
+import org.apache.spark.sql.jdbc.{JdbcDialect, JdbcDialects, JdbcType}
 
 object App {
     def main(args: Array[String]): Unit = {
         // Initialisation de Spark
         val spark = SparkSession.builder.appName("ETL").master("local[4]").getOrCreate()
+
+
+        // Paramètres de la connexion BD
+        // BD POSTGRESQL
+        val urlPostgreSQL = "jdbc:postgresql://stendhal:5432/tpid2020"
+        val connectionProporetiesPostgreSQL = new Properties()
+        connectionProporetiesPostgreSQL.setProperty("driver", "org.postgresql.Driver")
+        connectionProporetiesPostgreSQL.put("user", "tpid")
+        connectionProporetiesPostgreSQL.put("password", "tpid")
+        
+
+        // BD ORACLE
+        Class.forName("oracle.jdbc.driver.OracleDriver")
+        val urlOracle = "jdbc:oracle:thin:@stendhal:1521:enss2023"
+        val connectionPropertiesOracle = new Properties()
+        connectionPropertiesOracle.setProperty("driver", "oracle.jdbc.driver.OracleDriver")
+        connectionPropertiesOracle.setProperty("user", "ad578175")
+        connectionPropertiesOracle.setProperty("password", "ad578175")
+   
+      
+        val dialect = new OracleDialect
+        JdbcDialects.registerDialect(dialect)
 
         ///********** FICHIER CSV **********///
         // lecture du fichier tip.csv
@@ -164,29 +188,8 @@ object App {
         var checkin = checkin_info
             .withColumn("checkin_id", monotonically_increasing_id())
 
-
         
 
-        // Paramètres de la connexion BD
-        import java.util.Properties
-        // BD ORACLE
-        Class.forName("oracle.jdbc.driver.OracleDriver")
-        val urlOracle = "jdbc:oracle:thin:@stendhal:1521:enss2023"
-        val connectionPropertiesOracle = new Properties()
-        connectionPropertiesOracle.setProperty("driver", "oracle.jdbc.driver.OracleDriver")
-        connectionPropertiesOracle.setProperty("user", "ad578175")
-        connectionPropertiesOracle.setProperty("password", "ad578175")
-
-        // BD POSTGRESQL
-        val urlPostgreSQL = "jdbc:postgresql://stendhal:5432/tpid2020"
-        val connectionProporetiesPostgreSQL = new Properties()
-        connectionProporetiesPostgreSQL.setProperty("driver", "org.postgresql.Driver")
-        connectionProporetiesPostgreSQL.put("user", "tpid")
-        connectionProporetiesPostgreSQL.put("password", "tpid")
-        
-        import org.apache.spark.sql.jdbc.{JdbcDialect, JdbcDialects, JdbcType}
-        val dialect = new OracleDialect
-        JdbcDialects.registerDialect(dialect)
         spark.stop()
     }
 
